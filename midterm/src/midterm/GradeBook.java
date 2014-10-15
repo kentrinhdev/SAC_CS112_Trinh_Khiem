8151a238b248168c2b2e0b9d32d3e8a8ff14baf5
Ken Trinh <kentrinhdev@gmail.com>
2014-10-14 21:23:10 -0700

    KT
    SAC

---
+++ midterm/src/midterm/GradeBook.java
@@ -0,0 +1,13 @@
+
+
+import javax.swing.JOptionPane;
+
+public class GradeBook 
+{
+	// Display a welcome message to the GradeBook user.
+	public void displayMessage()
+	{
+		JOptionPane.showMessageDialog( null, "Welcome to the Grade Book!" );;
+	}
+	
+} // end class
